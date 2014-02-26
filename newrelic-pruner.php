03ab24798a2969769ab8a3b819ec66a697f8dc26
David Eriksson <david.eriksson@softhouse.se>
2014-02-26 08:33:57 +0100

    Only delete PHP applications...

--- newrelic-pruner.php
+++ newrelic-pruner.php
@@ -10,7 +10,7 @@ $newRelic = new NewRelic($apiKey);
 $applications = $newRelic->listApplications();
 
 foreach ($applications as $application) {
-    if (!$application->reporting) {
+    if (!$application->reporting && $application->language == 'php') {
         echo 'Deleting ' . $application->name . '...';
         $newRelic->deleteApplication($application->id);
         echo 'DONE' . PHP_EOL;
